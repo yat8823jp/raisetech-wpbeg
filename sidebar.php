fd01a70c93fb42d4b5a7a3b1c528a2cdc90bf653
yat8823jp <yat@yat-net.com>
2020-04-10 17:36:01 +0900

    sidebar-add-custom_field_archive

--- sidebar.php
+++ sidebar.php
@@ -21,19 +21,19 @@
 					'meta_query' => array(                  //カスタムフィールドに関するパラメーターをまとめた配列
 						'relation' => 'AND',                  //AND : 条件1,2をどちらも満たすもの
 
-						//条件1 ( 'color'というカスタムフィールドが 'red' もしくは 'blue' のもの )
+						//条件1 ( 'book_author' というカスタムフィールドが 'サンプル著者' のもの )
 						array(
-						  'key' => 'book_author',                   //カスタムフィールドのキーの指定
-						  'value' => array('サンプル著者'),     //カスタムフィールドの値の指定
+						  'key' => 'book_author',             //カスタムフィールドのキーの指定
+						  'value' => array( 'サンプル著者' ),    //カスタムフィールドの値の指定
 						  'type' => 'CHAR',                   //カスタムフィールドの値の型が何か教える
-						  'compare' => 'IN'                  //IN : 'value'パラメータで指定した値のいずれかに一致
+						  'compare' => 'IN'                   //IN : 'value'パラメータで指定した値のいずれかに一致
 						),
 
-						//条件2　（'price' の値が 1000 以下で、かつ、'size' が 'big' と一致するカスタムフィールドの値を持つもの）
+						//条件2　（ 'book_price' の値が 0 以上で、かつ、'book_isbn' が '' と一致しないカスタムフィールドの値を持つもの）
 						array(
 							'relation' => 'AND',              //AND : 条件A,Bを両方満たすもの
 
-							//条件2を形成する条件A (priceというカスタムフィールドの値が1000以下)
+							//条件2を形成する条件A (priceというカスタムフィールドの値が0以上)
 							array(
 							  'key' => 'book_price',
 							  'value' => 0,
@@ -41,20 +41,20 @@
 							  'compare' => '>'
 							),
 
-							//条件2を形成する条件B (sizeをいうカスタムフィールドの値がbigと一致)
+							//条件2を形成する条件B (sizeをいうカスタムフィールドの値が''と一致しない（空白でない） )
 							array(
 							  'key' => 'book_isbn',
 							  'value' => '',
 							  'compare' => '!='
 							)
 						)
-					  )
-					);
+					)
+				);
 				$wp_query = new WP_Query( $args );
-				$post_count  = $wp_query -> post_count;   //実際にそのページで取得した件数
-				$found_posts = $wp_query -> found_posts;  //条件に当てはまる全件数
-				echo "post_count" . $post_count . "<br>";
-				echo "found_posts" . $found_posts . "<br>";
+				// $post_count  = $wp_query -> post_count;   //実際にそのページで取得した件数
+				// $found_posts = $wp_query -> found_posts;  //条件に当てはまる全件数
+				// echo "post_count" . $post_count . "<br>";
+				// echo "found_posts" . $found_posts . "<br>";
 			?>
 			<ul>
 				<?php
